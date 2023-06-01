b02cd4e8b9b5b1a082526e752a91e5454d3ec977
imrookhan44 <91519591+imrookhan44@users.noreply.github.com>
2023-06-01 11:51:06 +0500

    Update Firebase.js

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -4,13 +4,13 @@ import "firebase/firestore";
 import "firebase/database";
 
 const firebaseConfig = {
- apiKey: "AIzaSyCsO4BWkn_GUxJhSbhf93PBmX2KvEKcOpg",
-  authDomain: "healthcare-7909e.firebaseapp.com",
-  projectId: "healthcare-7909e",
-  storageBucket: "healthcare-7909e.appspot.com",
-  messagingSenderId: "1010376615973",
-  appId: "1:1010376615973:web:95100d891929f1d66237cc",
-  measurementId: "G-M1JV5B97QV"
+ apiKey: "AIzaSyC9uXGXt1lrBQCEUIJBdql96aDbxP19ub4",
+  authDomain: "health-e5791.firebaseapp.com",
+  projectId: "health-e5791",
+  storageBucket: "health-e5791.appspot.com",
+  messagingSenderId: "472167689269",
+  appId: "1:472167689269:web:aa5a42f3af021e41fb957c",
+  measurementId: "G-MRJ70RJE54"
 };
 
 let InitFirebase;
